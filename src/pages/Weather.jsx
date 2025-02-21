ce117036d2f08c737b5a7ec0c984942c3e3ebf2d
MatheusGCruz <matheuskaze@gmail.com>
2025-02-21 18:31:20 -0300

    Added title

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,7 +1,7 @@
 function Weather (){
     
     return(
-        <iframe src="https://weather.antares.ninja/uberlandia" width="100%" height="100%" frameborder="0" ></iframe>
+        <iframe src="https://weather.antares.ninja/uberlandia" width="100%" height="100%" frameborder="0" title="Weather System webpage - VUE project"></iframe>
 
     )
 }
