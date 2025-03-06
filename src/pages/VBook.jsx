ee561e723f56ad8d69e8fcd4bd182b82d77af021
Matheus Cruz <matheuskaze@gmail.com>
2025-03-06 11:50:06 -0300

    Update VBook.jsx

--- src/pages/VBook.jsx
+++ src/pages/VBook.jsx
@@ -1,11 +1,11 @@
 import useScreenSize from '../functions/ScreenSize';
 
-function Weather (){
+function Vbook (){
     const screenSize = useScreenSize();
     
     return(
-        <iframe src="https://vbook.antares.ninja/"  style={{width:screenSize.fullWidth, height:screenSize.height}} frameborder="0" title="VBook - ReactJS + NodeJS system"></iframe>
+        <iframe src="https://vbook.antares.ninja/" title="VBook - ReactJS + NodeJS system"></iframe>
     )
 }
 
-export default Weather
+export default Vbook
