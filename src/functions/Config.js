5f8d69e7b32e27d72a61e039c50a7fa21760e02b
MatheusGCruz <matheuskaze@gmail.com>
2026-07-31 07:24:16 -0300

    Hotfix - videos links

--- src/functions/Config.js
+++ src/functions/Config.js
@@ -2,10 +2,10 @@ import { useState } from 'react';
 
 const useConfigs = () => {
   const [config] = useState({
-        videoFiles:"https://api.antares.ninja/video/files",
-        mkvVideos:"https://api.antares.ninja/videos/mkv",
+        videoFiles:"https://api.antares.ninja/videos/files",
+        mkvVideos:"https://api.antares.ninja/videos/mkv/",
         musicFiles:"https://api.antares.ninja/music/files",
-        videos:"https://api.antares.ninja/videos",
+        videos:"https://api.antares.ninja/videos/mp4/",
         music:"https://api.antares.ninja/music/",
         book:"https://api.antares.ninja/book"
   });
