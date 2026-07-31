8a6ec10b748e1d2d3f2d81325d050229dca21a7e
MatheusGCruz <matheuskaze@gmail.com>
2026-07-31 08:45:05 -0300

    Added - cast, audio(bugged), subtitles(bugged)

--- src/functions/Config.js
+++ src/functions/Config.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 const useConfigs = () => {
   const [config] = useState({
         videoFiles:"https://api.antares.ninja/videos/files",
+        videoTracks:"https://api.antares.ninja/videos/tracks/",
         mkvVideos:"https://api.antares.ninja/videos/mkv/",
         musicFiles:"https://api.antares.ninja/music/files",
         videos:"https://api.antares.ninja/videos/mp4/",
