50fb36949880cd0ebc32d6fc1a4b01f8c4e09c1c
MatheusGCruz <matheuskaze@gmail.com>
2026-07-31 18:15:49 -0300

    Adjusted - grafana dashboard

--- src/pages/Grafana.jsx
+++ src/pages/Grafana.jsx
@@ -4,7 +4,7 @@ function Weather (){
     const screenSize = useScreenSize();
     
     return(
-        <iframe src="https://grafana.antares.ninja/public-dashboards/25fc110ace1649618de7129ccd06f91e"  style={{width:screenSize.fullWidth, height:screenSize.height}} height="100em" frameBorder="0" title="Grafana - C# data collector"></iframe>
+        <iframe src="https://grafana.antares.ninja/public-dashboards/9aca30bb16b444f9a8ca23b2c894f122"  style={{width:screenSize.fullWidth, height:screenSize.height}} height="100em" frameBorder="0" title="Grafana - C# data collector"></iframe>
     )
 }
 
