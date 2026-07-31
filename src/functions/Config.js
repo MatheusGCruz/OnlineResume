0e80a859aa11c02415cb3fc27a34a8e33a896fd0
MatheusGCruz <matheuskaze@gmail.com>
2026-07-31 07:19:14 -0300

    Adjusted - videos links

--- src/functions/Config.js
+++ src/functions/Config.js
@@ -2,8 +2,8 @@ import { useState } from 'react';
 
 const useConfigs = () => {
   const [config] = useState({
-        videoFiles:"https://api.antares.ninja/videoFiles",
-        mkvVideos:"https://api.antares.ninja/mkv-videos",
+        videoFiles:"https://api.antares.ninja/video/files",
+        mkvVideos:"https://api.antares.ninja/videos/mkv",
         musicFiles:"https://api.antares.ninja/music/files",
         videos:"https://api.antares.ninja/videos",
         music:"https://api.antares.ninja/music/",
